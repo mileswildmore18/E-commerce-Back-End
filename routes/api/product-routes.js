5f90509efc1cff01a52923b19a7a13f634452958
Miles Wildmore <mileswildmore18@gmail.com>
2024-05-07 09:27:28 -0400

    Added a delete function for each route in case the user wants to delete the item.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -101,8 +101,17 @@ router.put('/:id', (req, res) => {
     });
 });
 
+ //deletes one product by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy ({
+    where :{
+    id: req.params.id,
+    },
+  })
+  .then((deletedProduct) => {
+    res.json(deletedProduct);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
